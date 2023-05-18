c58b5a74304b7c8eb85a836144d0902ecf14fdda
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-18 14:54:11 +0600

    dataTable

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import Form from './components/Form'
 import Posts from './components/Posts'
 
 function App() {
-
-
   return (
     <section className="main">
       <Form />
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "../contextAPI/Context";
+import { useDispatch } from "../contextAPI/Context";
+import DataTable from "./DataTable";
 
 export default function Posts() {
     const dispatch = useDispatch();
-    const posts = useSelector(state => state.posts);
     useEffect(() => {
         const loadPosts = async () => {
             const res = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -17,13 +17,7 @@ export default function Posts() {
     }, [dispatch])
     return (
         <div>
-            {
-                posts && posts.map(post => (
-                    <div key={post.id}>
-                        {post.title}
-                    </div>
-                ))
-            }
+            <DataTable />
         </div>
     )
 }
