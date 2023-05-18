ad1f5301a5f7039c26a045fd6a0c4d8c34ae8ac4
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-18 16:06:58 +0600

    toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css'
-import Form from './components/Form'
-import Posts from './components/Posts'
+import './App.css';
+import Form from './components/Form';
+import Posts from './components/Posts';
 
 function App() {
+
   return (
     <section className="main">
+
       <Form />
       <Posts />
     </section>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,8 @@
 import { Button, FormControl, TextField } from "@mui/material";
 import { useState } from "react";
+import { Toaster, toast, } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+
 import { useDispatch } from "../contextAPI/Context";
 import { btn, input } from "../styles/style";
 import { InputEvent } from "../types/types";
@@ -9,7 +12,7 @@ export default function Form() {
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   const dispatch = useDispatch();
-
+  const navigate = useNavigate()
   const submitter = (e: InputEvent) => {
     e.preventDefault();
 
@@ -17,9 +20,20 @@ export default function Form() {
       type: "ADD_USER",
       payload: { name, phone, email }
     })
+    if (name && phone && email) {
+      toast.success("User Added Successfully", { duration: 1500 });
+      setTimeout(() => { toast("You will be redirected to page 2", { icon: '↗️', }) }, 1000)
+    } else {
+      toast.error("Please fill all the fields",);
+    }
   };
+
   return (
     <div className="form-container">
+      <div>
+        <Toaster />
+      </div>
+
       <form onSubmit={submitter}>
         <FormControl sx={{ minWidth: "400px" }}>
           <TextField
